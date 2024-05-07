c6269fa67a44d8cabee388165d7c39e631dc421c
LLCoelh0 <leonardo-coelho1@hotmail.com>
2024-05-06 23:37:31 -0300

    commit final

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,16 +1,19 @@
+menu = '''
+[D]eposit
+[W]ithdraw
+[S]tatement
+[E]xit
+Enter with a option: '''
+
 balance = 0
+limit = 500
 statement = []
 number_of_withdraws = 0
 withdraw_limit = 3
 
 while True:
-    operation = input('''
-[D]eposit
-[W]ithdraw
-[S]tatement
-[E]xit
-Enter with a option: ''').lower()
-    
+    operation = input(menu).lower()
+
     if operation == 'd':
         deposit = float(input('Enter with the value for Deposit: '))
         if deposit <= 0:
@@ -23,12 +26,15 @@ Enter with a option: ''').lower()
         if number_of_withdraws < withdraw_limit:
             withdraw = float(input('Enter with the value for Withdraw: '))
             if withdraw > 0:
-                if balance - withdraw >= 0:
-                    balance -= withdraw
-                    statement.append(f'-U$ {withdraw:.2f}')
-                    number_of_withdraws += 1
+                if withdraw <= limit:
+                    if balance - withdraw >= 0:
+                        balance -= withdraw
+                        statement.append(f'-U$ {withdraw:.2f}')
+                        number_of_withdraws += 1
+                    else:
+                        print('Not enough balance, try other value')
                 else:
-                    print('Not enough balance, try other value')
+                    print(f'Withdraw limit exceeded choose a value lower than {limit}')
             else:
                 print('Enter with a valid value!')
         else:
